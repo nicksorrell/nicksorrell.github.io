d5c7e4e02a748a34768e90f50c90f7fec49e0cfc
Nick Sorrell <nicksorrell@gmail.com>
2015-05-19 16:18:57 -0400

    first commit

---
+++ backend/config.php
@@ -0,0 +1,11 @@
+<?php
+
+	$readonly = false;
+	$version = '6.00';
+	
+	$mysql['server'] = 'localhost';
+	$mysql['username'] = '******';
+	$mysql['password'] = '******';
+	$mysql['database'] = '******';
+
+	
